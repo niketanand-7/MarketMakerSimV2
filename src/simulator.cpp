4b1ec236fc5d27ccfc33a9caa71b91396010a422
niketanand-7 <niketanand7@gmail.com>
2024-07-07 21:38:10 -0400

    file structures

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <chrono>
+#include <cmath>
 
 using namespace std;
+using namespace std::chrono;
 
 int main(){
-    std::cout << "Hello World" << std::endl;
+    
+    auto start = high_resolution_clock::now();
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end - start);
+    cout << "Time taken by function: "
+         << duration.count() << " microseconds" << endl;
     return 0;
 }
