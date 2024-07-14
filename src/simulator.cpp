78cdc9b403935868ef5f9cec24a7a6d31a2bbdb1
niketanand-7 <niketanand7@gmail.com>
2024-07-14 00:24:08 -0500

    some errors, but more file structure and starter code

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -5,11 +5,21 @@
 #include <ctime>
 #include <chrono>
 #include <cmath>
+#include "../include/OrderBook/OrderBook.h"
 
 using namespace std;
 using namespace std::chrono;
 
 int main(){
+    OrderBook orderBook();
+    dayOrder = orderBook.getOrder();
+
+    // Get trading time stamps that are after the first 30 minutes
+    auto tradingTimeStamps = orderBook.getTradingTimeStamps();
+    int startIndex = tradingTimeStamps.size() > 30 ? 30: 0;
+    tradingTimeStamps.erase(tradingTimeStamps.begin(), tradingTimeStamps.begin() + startIndex);
+
+
     auto start = high_resolution_clock::now();
     auto end = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(end - start);
